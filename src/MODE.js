cb4f4f390b8636000b768603d64ac6c850a58ac4
osheow <osheow@lion.lmu.edu>
2024-02-16 22:39:00 -0800

    fixed node error

--- src/MODE.js
+++ src/MODE.js
@@ -1,7 +1,8 @@
 import * as ohm from 'ohm-js'
 import fs from 'fs'
 
-const grammar = ohm.grammar(fs.readFileSync('./MODE.ohm', 'utf-8'))
+const grammar = ohm.grammar(fs.readFileSync('./src/MODE.ohm', 'utf-8'))
 export function matches(name, s) {
     return grammar.match(s).succeeded()
 }
+
