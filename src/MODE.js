f00d3219244e3c8ba1cc16e895b3b7254a004199
osheow <osheow@lion.lmu.edu>
2024-02-16 22:53:33 -0800

    added Syntax is Okay

--- src/MODE.js
+++ src/MODE.js
@@ -1,8 +1,18 @@
 import * as ohm from 'ohm-js'
-import fs from 'fs'
+import fs from 'fs';
 
-const grammar = ohm.grammar(fs.readFileSync('./src/MODE.ohm', 'utf-8'))
-export function matches(name, s) {
-    return grammar.match(s).succeeded()
+const fileName = process.argv[2];
+
+if (!fileName) {
+    console.error('Usage: node src/MODE.js filename');
+    process.exit(1);
 }
 
+try {
+    const fileContent = fs.readFileSync('./src/MODE.ohm', 'utf8');
+    const grammar = ohm.grammar(fileContent);
+    console.log('Syntax is okay');
+} catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+}
