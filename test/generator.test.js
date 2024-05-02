0c5b581eae73c2353d7195a5d5c8bb5ba910c63e
Ray Toal <rtoal@lmu.edu>
2024-05-01 17:07:25 -0700

    Generator

--- test/generator.test.js
+++ test/generator.test.js
@@ -1,39 +1,39 @@
-import assert from "node:assert/strict"
-import parse from "../src/parser.js"
-import analyze from "../src/analyzer.js"
-import optimize from "../src/optimizer.js"
-import generate from "../src/generator.js"
+import assert from 'node:assert/strict'
+import parse from '../src/parser.js'
+import analyze from '../src/analyzer.js'
+import optimize from '../src/optimizer.js'
+import generate from '../src/generator.js'
 
 function dedent(s) {
-  return `${s}`.replace(/(?<=\n)\s+/g, "").trim()
+    return `${s}`.replace(/(?<=\n)\s+/g, '').trim()
 }
 
 const fixtures = [
-  {
-    name: "small",
-    source: `
+    {
+        name: 'small',
+        source: `
       auto x = 3 * 7;
       x = x + 1;
       x = x - 1;
       auto y = true;
     `,
-    expected: dedent`
+        expected: dedent`
       let x_1 = 21;
       x_1 = (x_1 + 1);
       x_1 = (x_1 - 1);
       let y_2 = true;
     `,
-  },
-  {
-    name: "if",
-    source: `
+    },
+    {
+        name: 'if',
+        source: `
       auto x = 0;
       if x == 0 { print "1"; }
       if x == 0 { print 1; } else { print 2; }
       if x == 0 { print 1; } else if x == 2 { print 3; }
       if x == 0 { print 1; } else if x == 2 { print 3; } else { print 4; }
     `,
-    expected: dedent`
+        expected: dedent`
       let x_1 = 0;
       if ((x_1 === 0)) {
         console.log("1");
@@ -58,10 +58,10 @@ const fixtures = [
           console.log(4);
         }
     `,
-  },
-  {
-    name: "while",
-    source: `
+    },
+    {
+        name: 'while',
+        source: `
       auto x = 0;
       while x < 5 {
         auto y = 0;
@@ -73,7 +73,7 @@ const fixtures = [
         x = x + 1;
       }
     `,
-    expected: dedent`
+        expected: dedent`
       let x_1 = 0;
       while ((x_1 < 5)) {
         let y_2 = 0;
@@ -85,10 +85,10 @@ const fixtures = [
         x_1 = (x_1 + 1);
       }
     `,
-  },
-  {
-    name: "functions",
-    source: `
+    },
+    {
+        name: 'functions',
+        source: `
       auto z = 0.5;
       func f(x: float, y: bool) {
         print x;
@@ -98,7 +98,7 @@ const fixtures = [
         return false;
       }
     `,
-    expected: dedent`
+        expected: dedent`
       let z_1 = 0.5;
       function f_2(x_3, y_4) {
         console.log(x_3);
@@ -108,32 +108,32 @@ const fixtures = [
         return false;
       }
     `,
-  },
-  {
-    name: "arrays",
-    source: `
+    },
+    {
+        name: 'arrays',
+        source: `
       auto a = [true, false, true];
       auto b = [10, 20, 30];
       auto c = int[]();
       print a[1];
       print b[1];
     `,
-    expected: dedent`
+        expected: dedent`
       let a_1 = [true,false,true];
       let b_2 = [10,20,30];
       let c_3 = [];
       console.log(a_1[1]);
       console.log(b_2[1]);
     `,
-  },
-  {
-    name: "classes",
-    source: `
+    },
+    {
+        name: 'classes',
+        source: `
       class S { x: int; };
       auto x = S(3);
       print(x.x);
     `,
-    expected: dedent`
+        expected: dedent`
       class S {
       constructor(x_1) {
       this["x_1"] = x_1;
@@ -142,55 +142,50 @@ const fixtures = [
       let x_2 = new S(3);
       console.log((x_2["x_1"]));
     `,
-  },
-  {
-    name: "function calls",
-    source: `
+    },
+    {
+        name: 'function calls',
+        source: `
       func f(x:int, y:int): void {
         print x + y;
       }
       f(3, 4);
     `,
-    expected: dedent`
+        expected: dedent`
       function f_1(x_2, y_3) {
-        console.log(x_2 + y_3);
+        console.log((x_2 + y_3));
       }
       f_1(3, 4);
     `,
-  }
-  
-  
-  
-  
+    },
 
-
-  // Not synactically correct yet - loop over array not implemented
-  // {
-  //   name: "for loops",
-  //   source:`
-  //     for i in 1 .. 50 {
-  //       print i;
-  //     }
-  //     for j in [10, 20, 30] {
-  //       print j;
-  //     }
-  //   `,
-  //   expected: dedent`
-  //     for (let i_1 = 1; i_1 < 50; i_1++) {
-  //       console.log(i_1);
-  //     }
-  //     for (let j_2 of [10,20,30]) {
-  //       console.log(j_2);
-  //     }
-  //   `,
-  // },
+    // Not synactically correct yet - loop over array not implemented
+    // {
+    //   name: "for loops",
+    //   source:`
+    //     for i in 1 .. 50 {
+    //       print i;
+    //     }
+    //     for j in [10, 20, 30] {
+    //       print j;
+    //     }
+    //   `,
+    //   expected: dedent`
+    //     for (let i_1 = 1; i_1 < 50; i_1++) {
+    //       console.log(i_1);
+    //     }
+    //     for (let j_2 of [10,20,30]) {
+    //       console.log(j_2);
+    //     }
+    //   `,
+    // },
 ]
 
-describe("The code generator", () => {
-  for (const fixture of fixtures) {
-    it(`produces expected js output for the ${fixture.name} program`, () => {
-      const actual = generate(optimize(analyze(parse(fixture.source))))
-      assert.deepEqual(actual, fixture.expected)
-    })
-  }
+describe('The code generator', () => {
+    for (const fixture of fixtures) {
+        it(`produces expected js output for the ${fixture.name} program`, () => {
+            const actual = generate(optimize(analyze(parse(fixture.source))))
+            assert.deepEqual(actual, fixture.expected)
+        })
+    }
 })
